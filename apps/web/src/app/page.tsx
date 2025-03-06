1c9153a795ea9e8d4c56550493da1538304693f0
Yi Tam <tam.yi3d@gmail.com>
2025-03-06 22:26:08 +0100

    add example user query and mutations

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,17 +1,33 @@
 'use client'
 
-import {trpcClient, useTRPC} from "@/utils/trpcClient";
-import {useEffect, useState} from "react";
-import {RouterOutput} from "@apps/api";
-import { useQuery } from "@tanstack/react-query";
+import {useTRPC} from "@/utils/trpcClient";
+import {useEffect} from "react";
+import {useMutation, useQuery} from "@tanstack/react-query";
 
 export default function Page() {
   const trpc = useTRPC();
 
-  const {data} = useQuery(trpc.user.list.queryOptions())
+  const {data, refetch} = useQuery(trpc.user.list.queryOptions())
+  const {mutate, data: createData} = useMutation(trpc.user.create.mutationOptions({
+    onSuccess(data) {
+      refetch()
+    }
+  }))
+
+  useEffect(() => {
+    mutate({
+      firstName: 'John',
+      lastName: 'Doe5',
+      email: 'john.doe5@example.com',
+      password: 'password',
+    })
+  }, []);
 
   return <div>
     <h1>Hello, world!</h1>
-    {data?.map(user => <p key={user.id}>{user.name}</p>)}
+    {
+      createData && <p>Created user: {createData.firstName} {createData.lastName}</p>
+    }
+    {data?.map(user => <p key={user.id}>{user.firstName} {user.lastName}</p>)}
   </div>
 }
