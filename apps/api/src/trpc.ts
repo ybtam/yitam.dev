26aac44535c032e49f6c52b921cc7aab465d504c
Yi Tam <tam.yi3d@gmail.com>
2025-03-07 19:34:47 +0100

    use one server not two

--- apps/api/src/trpc.ts
+++ apps/api/src/trpc.ts
@@ -5,30 +5,20 @@ import { AuthContext } from './context/auth-context.ts';
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
-const t = initTRPC.create({});
-
-/**
- * Export reusable router and procedure helpers
- * that can be used throughout the router
- */
+const t = initTRPC.context<AuthContext>().create({});
 export const router = t.router;
 export const publicProcedure = t.procedure;
 
-export const authContext = initTRPC.context<AuthContext>().create();
-// you can reuse this for any procedure
-export const protectedProcedure = authContext.procedure.use(
+export const protectedProcedure = t.procedure.use(
   async function isAuthed(opts) {
     const { ctx } = opts;
-    // `ctx.user` is nullable
     if (!ctx.user) {
       //     ^?
       throw new TRPCError({ code: 'UNAUTHORIZED' });
     }
     return opts.next({
       ctx: {
-        // ✅ user value is known to be non-null now
         user: ctx.user,
-        // ^?
       },
     });
   },
