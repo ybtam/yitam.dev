ff04bc8b50359afd53fc79a1006a8e53fca41318
Yi Tam <tam.yi3d@gmail.com>
2025-03-07 21:21:04 +0100

    export zod schema

--- apps/api/src/routes/auth/mutation.ts
+++ apps/api/src/routes/auth/mutation.ts
@@ -1,13 +1,14 @@
-import {publicProcedure} from "../../trpc.js";
+import {publicProcedure} from "../../trpc.ts";
 import {z} from "zod";
-import {generateToken, UserPayload} from "./util.js";
+import {generateToken, UserPayload} from "./util.ts";
+import {loginInputSchema} from "./schema.js";
 
 const login=  publicProcedure
-  .input(z.object({ email: z.string(), password: z.string() }))
+  .input(loginInputSchema)
   .output(z.object({ token: z.string() }))
   .mutation(async ({ input }) => {
     // Authenticate user against your database or authentication service
-    if (input.email === 'test@example.com' && input.password === 'password') {
+    if (input.email === 'test@test.test' && input.password === 'test') {
       const payload: UserPayload = { userId: '123', email: input.email };
       const token = generateToken(payload);
       return { token };
